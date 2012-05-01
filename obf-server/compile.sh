85245510e8c9af57f66bdaeb830618dd84f1f4e3
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-04-30 21:00:37 -0700

    Attempt to compile obfuscated source

---
+++ obf-server/compile.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+CLASSPATH=../minecraft_server_1.2.5.jar:../../industrialcraft-2-server_1.95b.jar:../../ModLoader.zip:../../minecraftforge-server-3.1.2.90.zip javac  `find . -name '*.java'`
